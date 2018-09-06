91d179bf2fc6b6c143cdc142376ff609a26b869a
Edward Brennan <EdMan1022@gmail.com>
2018-09-06 09:54:26 -0400

    Example that tries to combine clearing the terminal and printing colored output

--- printing_scrap/multi_line_print.py
+++ printing_scrap/multi_line_print.py
@@ -5,24 +5,26 @@ import time
 def clear_lines(n_lines):
     default_string = "\033[99m"
 
-    for line in range(n_lines):
-        sys.stdout.write("\033[F")  # Move cursor up one line
-        sys.stdout.write("\033[K")  # Clear to end of line
-    sys.stdout.write(default_string)  # Reset coloring
+    print(chr(27) + "[2J")
+
+    # for line in range(n_lines):
+    #     sys.stdout.write("\033[F")  # Move cursor up one line
+    #     sys.stdout.write("\033[K")  # Clear to end of line
 
 
 def character(n_chars):
 
     red_string = "\033[91m"
     default_string = "\033[99m"
+    black_string = "\033[90m"
+    white_string = "\033[97m"
 
-    sys.stdout.write(default_string)
-    print("=====")
-    sys.stdout.write(red_string)
-    print("{}".format(n_chars * ">"))
-    sys.stdout.write(default_string)
-    print("=====")
-    sys.stdout.write(default_string)
+    print("{}=====".format(default_string))
+    # sys.stdout.write(red_string)
+    print("{}{}".format(red_string, n_chars * ">"))
+    # sys.stdout.write(default_string)
+    print("{}=====".format(default_string))
+    # sys.stdout.write(default_string)
 
     time.sleep(.5)
 
@@ -30,7 +32,7 @@ def character(n_chars):
 if __name__ == "__main__":
     for i in range(5):
         character(i)
-        clear_lines(3)
+        # clear_lines(3)
 
     character(i + 1)
 
